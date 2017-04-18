0aa34700c191d158ea9e281ee30232be8fcc16c1
Siewert308SW <s.j.lameijer@gmail.com>
2017-04-18 06:54:00 +0200

    Optimized buienradar.lua

--- domoticz/scripts/lua/timers/buienradar.lua
+++ domoticz/scripts/lua/timers/buienradar.lua
@@ -4,13 +4,11 @@
 	@ buienradar.lua
 	@ author	: Siewert Lameijer
 	@ since		: 17-4-2017
-	@ updated	: 17-4-2017
+	@ updated	: 18-4-2017
 	@ Script for predicting rainfall in the upcoming hours
 	
 -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 --]]
-	
-local m = os.date('%M')
 
 --
 -- **********************************************************
@@ -21,87 +19,46 @@ local m = os.date('%M')
 	local RainTextIDX = 192   --your domoticz text device
 	local Rain60TextIDX = 193   --your domoticz text device	
 	local Rain120TextIDX = 194   --your domoticz text device		
-
+	local m = os.date('%M')
 	totalrain = 0
 	rainlines = 0
 
 
 
-if (m % 30 == 0) then -- run once in 30 min
+if (m % 15 == 0) then -- run once in 15 min
 
 	   minuten=30
 	   minuten60=60
-	   minuten120=110
+	   minuten120=100
 	   
 	   RainPrediction = IsItGonnaRain(minuten)
-	   RainmmHour=10^((RainPrediction-109)/32)
-	   
-	   RainPrediction60 = IsItGonnaRain(minuten60)
-	   RainmmHour=10^((RainPrediction60-109)/32)
-
-	   RainPrediction120 = IsItGonnaRain(minuten120)
-	   RainmmHour=10^((RainPrediction120-109)/32)	   
-
---
--- **********************************************************
--- Buienradar - komende 30 minuten
--- **********************************************************
---	   
-
-	   if (RainPrediction > 5  ) then
-		  verw = 3
-		  RainPredictionText=('Komende 30min wordt er een buitje verwacht!')
-		  
-	   elseif (RainPrediction > 13  ) then
-		  verw = 3
-		  RainPredictionText=('Komende 30min ('..round(RainmmHour, 1)..' mm) regen verwacht!')	  
-	   else
-		  verw = 2
-		  RainPredictionText=('Komende 30min blijft het voorlopig droog!')
-	   end
-
---
--- **********************************************************
--- Buienradar - komende 60 minuten
--- **********************************************************
---
-	   
-	   if (RainPrediction60 > 5  ) then
-		  verw = 3
-		  RainPredictionText60=('Komende uur wordt er een buitje verwacht!')
-		  
-	   elseif (RainPrediction60 > 13  ) then
-		  verw = 3
-		  RainPredictionText60=('Komende uur ('..round(RainmmHour, 1)..' mm) regen verwacht!')	  
-	   else
-		  verw = 2
-		  RainPredictionText60=('Komende uur blijft het voorlopig droog!')
-	   end
-
---
--- **********************************************************
--- Buienradar - komende 120 minuten
--- **********************************************************
---
-	   
-	   if (RainPrediction120 > 10  ) then
-		  verw = 3
-		  RainPredictionText120=('Na het komende uur word er nog een buitje verwacht!')
-		  
-	   elseif (RainPrediction120 > 13  ) then
-		  verw = 3
-		  RainPredictionText120=('Na het komende uur word er nog ('..round(RainmmHour, 1)..' mm) regen verwacht!')	  
-	   else
-		  verw = 2
-		  RainPredictionText120=('Na het komende uur blijft het voorlopig droog!')
-	   end		   
+	   RainmmHour=10^((RainPrediction-109)/32)	   
 
 --
 -- **********************************************************
 -- Buienradar - Update virtual text device
 -- **********************************************************
---
-	   
+--	   
+
+	   if (RainPrediction > 5  ) then
+		  verw = 3
+		RainPredictionText=('Komende 30min wordt er een buitje verwacht!')
+			RainPredictionText60=('Komende uur wordt er een buitje verwacht!')
+				RainPredictionText120=('Na het komende uur word er een buitje verwacht!')			  
+	   elseif (RainPrediction > 12  ) then
+		  verw = 2
+		RainPredictionText=('Komende 30min ('..round(RainmmHour, 1)..' mm) regen verwacht!')
+			RainPredictionText60=('Komende uur ('..round(RainmmHour, 1)..' mm) regen verwacht!')
+				RainPredictionText120=('Na het komende uur word er ('..round(RainmmHour, 1)..' mm) regen verwacht!')			
+			
+	   else
+		  verw = 1
+		RainPredictionText=('Komende 30min blijft het voorlopig droog!')
+		  	RainPredictionText60=('Komende uur blijft het voorlopig droog!')
+				RainPredictionText120=('Na het komende uur blijft het voorlopig droog!')			
+	   end	   
+
+
 	   commandArray[1] = {['UpdateDevice'] = RainTextIDX .. '|0|' .. tostring(RainPredictionText)}	   
 	   commandArray[2] = {['UpdateDevice'] = Rain60TextIDX .. '|0|' .. tostring(RainPredictionText60)}
 	   commandArray[3] = {['UpdateDevice'] = Rain120TextIDX .. '|0|' .. tostring(RainPredictionText120)}	   
