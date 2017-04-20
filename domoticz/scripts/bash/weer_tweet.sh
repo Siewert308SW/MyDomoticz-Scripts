f0469c4d899a72157bffe0f333d7df8206c391c8
Siewert308SW <s.j.lameijer@gmail.com>
2017-04-20 21:56:18 +0200

    Fixed chill temp, didn't show number after decimal Added check to see if tweet isn't exceeding 140 chars Optimized tweet text to not exceed 140 chars when all values are used

--- domoticz/scripts/bash/weer_tweet.sh
+++ domoticz/scripts/bash/weer_tweet.sh
@@ -21,7 +21,7 @@ hygro=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt | awk -F: 
 
 rain=`curl "http://127.0.0.1:8080/json.htm?type=devices&rid=186"`
 echo $rain > /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt
-rain=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt | awk -F: '{print $33, $34}' | awk '{print $3}' | sed 's/\"//g' | sed 's/,//g'`
+rain=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt | awk -F: '{print $33, $34}' | awk '{printf("%.1f\n", $3)}' | sed 's/\"//g' | sed 's/,//g'`
 
 rainforecast=`curl "http://127.0.0.1:8080/json.htm?type=devices&rid=192"`
 echo $rainforecast > /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt
@@ -34,7 +34,7 @@ speed=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt | awk -F: 
 
 chill=`curl "http://127.0.0.1:8080/json.htm?type=devices&rid=166"`
 echo $chill > /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt
-chill=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt | awk -F: '{print $15, $16}' | awk '{print $3}' | cut -c 1-3 | sed 's/\"//g' | sed 's/,//g'`
+chill=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt | awk -F: '{print $15, $16}' | awk '{printf("%.1f\n", $3)}' | sed 's/\"//g' | sed 's/,//g'`
 
 baro=`curl "http://127.0.0.1:8080/json.htm?type=devices&rid=165"`
 echo $baro > /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt
@@ -54,12 +54,17 @@ uv=`curl "http://127.0.0.1:8080/json.htm?type=devices&rid=167"`
 echo $uv > /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt
 uv=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data.txt | awk -F: '{print $16, $17}' | awk '{print $3}' | cut -c 1-2 | sed 's/\"//g' | sed 's/,//g'`
 
-echo -ne "#Zoutkamp |"$timestamp":00| "$rainforecast"\nTemp:"$temperature"C | Gevoel:"$chill"C\nVocht:"$hygro"%\nNeerslag:"$rain"mm\nWind:"$dir" "$speed"m/s\nBaro:"$baro"hPa\n"$zicht"UV:"$uv"" > /mnt/storage/domoticz_scripts/weer_tweets/weather-tweet.txt
+echo -ne "#Zoutkamp |"$timestamp"u| "$rainforecast"\nTemp:"$temperature"°C | Gevoel:"$chill"°C\nVocht:"$hygro"%\nNeerslag:"$rain"mm\nWind:"$dir" "$speed"m/s\nBaro:"$baro"hPa\n"$zicht"UV:"$uv"" > /mnt/storage/domoticz_scripts/weer_tweets/weather-tweet.txt
 
-#lightning=`curl "http://127.0.0.1:8080/json.htm?type=devices&rid=199"`
-#echo $lightning > /mnt/storage/domoticz_scripts/weer_tweets/weather-data2.txt
-#lightning=`cat /mnt/storage/domoticz_scripts/weer_tweets/weather-data2.txt | grep CounterToday | awk '{print $40}' | sed 's/\"//g' | sed 's/,//g'`
+tweet_text="/mnt/storage/domoticz_scripts/weer_tweets/weather-tweet.txt"
 
+if [ $(cat "$tweet_text" | wc -c) -gt 140 ]; then
+   echo "Tweet niet verzonden, je hebt meer dan 140 tekens!" && exit 1
+elif [ "$tweet_text" == "" ]; then
+   echo "Er is niks om te tweeten!" && exit 1
+fi
+
+### tweet
 sudo python /home/pi/domoticz/scripts/python/weer_tweet.py
 sleep 5
 exit
