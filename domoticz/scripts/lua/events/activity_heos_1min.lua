f8c5c5345dcc5cdabd46121caf498c969161956d
Siewert308SW <Siewert308SW@users.noreply.github.com>
2019-01-18 21:13:26 +0100

    Renamed script and optimized event

--- domoticz/scripts/lua/events/activity_heos_1min.lua
+++ domoticz/scripts/lua/events/activity_heos_1min.lua
@@ -4,11 +4,15 @@
 	@ activity_heos.lua
 	@ author	: Siewert Lameijer
 	@ since		: 1-1-2015
-	@ updated	: 15-01-2019
+	@ updated	: 17-01-2019
 	@ Simple Lua script to turn HEOS Speaker OFF
 
 -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 --]]
 	if devicechanged[media_device.tv] == 'On' or devicechanged[someone.home] == 'Off' then
+	heos('stop')
+	end
+	
+	if devicechanged[timed.trigger] and powerusage(watt.tvcorner) >= watt.tvcorner_usage and otherdevices[media_device.tv] == 'Off' then
 	heos('stop')
 	end
